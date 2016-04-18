601f076c3ff8ba8c1770a85d9d52c64e08cd347e
iamlow <iamlow@neighbor21.co.kr>
2016-04-18 18:20:16 +0900

    100 쓰레드를 생성하여 여러개의 쓰레드가 하나의 파일에 로그를 작성하도록 수정

--- log/Main.cpp
+++ log/Main.cpp
@@ -3,24 +3,36 @@
 #include <cstdlib>
 #include <string>
 
+#include <boost/thread.hpp>
+
 #include "Logger.hpp"
+
+void run()
+{
+	common::Logger::get_const_instance();
+
+	for (int i = 0; i < 20; i++) {
+		LOGV << boost::this_thread::get_id() <<": A 한글 message!";
+	    LOGD << boost::this_thread::get_id() <<": A 한글 message!";
+		LOGI << boost::this_thread::get_id() <<": A 한글 message!";
+		LOGW << boost::this_thread::get_id() <<": A 한글 message!";
+		LOGE << boost::this_thread::get_id() <<": A 한글 message!";
+		LOGF << boost::this_thread::get_id() <<": A 한글 message!";
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	::_putenv_s("LOG_PATH", "D:\\");
 	::_putenv_s("LOG_LEVEL", "DEBUG");
-	::_putenv_s("LOG_MAXFILESIZE", "1000");
-	::_putenv_s("LOG_MAXFILES", "2");
+	::_putenv_s("LOG_MAXFILESIZE", "10000");
+	::_putenv_s("LOG_MAXFILES", "10");
 
-	common::Logger::get_const_instance();
-	for (int i = 0; i < 20; i++) {
-		LOGV << "A 한글 message!";
-	    LOGD << "A 한글 message!";
-		LOGI << "A 한글 message!";
-		LOGW << "A 한글 message!";
-		LOGE << "A 한글 message!";
-		LOGF << "A 한글 message!";
+	boost::thread_group threads;
+	for (int i = 0; i < 100; ++i) {
+		threads.create_thread(boost::bind(&run));
 	}
+	threads.join_all();
 
     return 0;
 }
